c468cda6cac3feeb443c93b566dc94e0ff9c618a
Santiago León <85116254+Sant1001@users.noreply.github.com>
2021-11-20 20:46:47 -0600

    Update Act4_2.cpp

--- Act4_2.cpp
+++ Act4_2.cpp
@@ -11,6 +11,7 @@ public:
     void addedge(int r, int s);
     bool isTree();
     void topologicalSort();
+	void loadGraph(Graph &g,int m);
 };
  
 Graph::Graph(int V)
@@ -32,7 +33,9 @@ void Graph::addedge(int r, int s)
 
 void Graph::topologicalSort()
 {
+	vector <char> alfa= {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
     vector<int> in_degree(V, 0);
+	char a;
     for (int u = 0; u < V; u++) {
         list<int>::iterator itr;
         for (itr = adj[u].begin();
@@ -61,7 +64,7 @@ void Graph::topologicalSort()
         return;
     }
     for (int i = 1; i < top_order.size(); i++)
-        cout << top_order[i] << " ";
+        cout << alfa[top_order[i]-1] << " ";
     cout << endl;
 }
 
@@ -94,35 +97,40 @@ bool Graph::isTree()
  
     return true;
 }
-int main()
-{
-    Graph g(14);
-    g.addEdge(1, 4);
-    g.addEdge(2, 4);
-    g.addEdge(3, 1);
-    g.addEdge(3, 2);
-    g.addEdge(4, 7);
-    g.addEdge(4, 8);
-    g.addEdge(5, 1);
-    g.addEdge(5, 4);
-    g.addEdge(5, 6);
-    g.addEdge(6, 10);
-    g.addEdge(6, 11);
-    g.addEdge(7, 9);
-    g.addEdge(8, 9);
-    g.addEdge(8, 10);
-    g.addEdge(9, 12);
-    g.addEdge(10, 12);
-    g.addEdge(10, 13);
-    g.addEdge(11, 10);
-    g.isTree()? cout << "Graph is Tree\n":
-                cout << "Graph is not Tree\n";
- 
-    cout << "Following is a Topological Sort of\n";
+
+void Graph::loadGraph(Graph &g,int m){
+	vector <char> alfa= {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
+	char a, b;
+	int a1,b1;
+	for (int i = 0; i < m; i++){
+		cin >> a >> b;
+		for (int j = 0; j < m; j++){
+			if (a==alfa[j]){
+				a1 = j+1;
+			}
+			if (b==alfa[j]){
+				b1 = j+1;
+			}
+		}
+		g.addEdge(a1,b1);
+	}
+}
+
+int main(){
+	int n, m;
+	cin >> n >> m;
+	if(n<=0){
+		cout << -1 << endl;
+	}else{
+		n=n+1;
+    Graph g((n));
+	g.loadGraph(g,m);
+	if (n<=1){
+		cout << boolalpha << false << endl;
+	}else{
+		cout << boolalpha << g.isTree() << endl;
+	}
     g.topologicalSort();
- 
+	}
     return 0;
 }
-//1A 2B 3C 4D 5E 6F 7G 8H 9I 10J 11K 12L 13M
-
-//3 5 2 1 6 4 11 7 8 9 10 12 13
